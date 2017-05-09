a64be30cb600aa0d1cbcc0b62bb56846c0cdfee0
Lauren Ancona <lauren.ancona@gsa.gov>
2017-05-09 14:01:09 -0400

    make privacy link text larger

--- recruiter.js
+++ recruiter.js
@@ -17,7 +17,7 @@ N.gaf = {
 		'#gaf-submit{text-decoration:none;margin-left:auto;margin-right:auto;display:block;}' +
 		'#gaf-submit:hover{opacity:.7}' +
 		'#gaf-dialog-close{position:absolute;top:0%;right:0%;padding:10px;font-size:24px;color:rgba(0,0,0,.3);line-height:1;text-decoration:none}' +
-		'#gaf-dialog-privacy{position:absolute;bottom:1%;font-size:.7rem;color:#aeb0b5;}' +
+		'#gaf-dialog-privacy{position:absolute;bottom:1%;font-size:.9rem;color:#aeb0b5;}' +
 
 		'@media only screen and (max-width:600px){' +
 			'#gaf-dialog{left:10%;width:80%}' +
