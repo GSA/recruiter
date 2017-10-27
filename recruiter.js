7c09aea1b6ccd170c08f33fbb85679802aed84b1
Lauren Ancona <ancona.lauren@gmail.com>
2017-10-27 01:19:09 -0400

    Update recruiter.js to match version loaded in GTM

--- recruiter.js
+++ recruiter.js
@@ -1,18 +1,20 @@
-// All form components are namespaced under 'fba' = 'Feedback Analytics'
-
+<script>
 (function ( d, N ) { 'use strict';
+  // All form components are namespaced under 'fba' = 'Feedback Analytics'
 	N.fba = {
-		css: 	'#fba-button{position:fixed;bottom:0;right:50px;background:#02bfe7;color:#fff;padding:5px 10px;font-size:1em;text-decoration:none;z-index:999999999}' +
-			'#fba-dialog{position:fixed;top:5%;left:33%;right:33%;min-width:350px;background:#f1f1f1;padding:20px;z-index:999999999}' +
-			'#fba-dialog h3{margin-top:1em}' +
-			'#fba-text-name,#fba-text-email{max-width:100%!important;font-size:100%}' +
-			'#fba-dialog-close{position:absolute;top:0%;right:0%;padding:0px 3px;font-size:24px;color:#5b616b;background:none;line-height:1;text-decoration:none}' +
-			'#fba-dialog-privacy{position:absolute;bottom:1%;font-size:1.2rem;color:#aeb0b5;}' +
-
-			'@media only screen and (max-width:400px){' +
-				'#fba-dialog{left:10%;width:80%}' +
-				'#fba-dialog-close{right:10%}' +
-			'}',
+		css:
+		'#fba-overlay.is-active{ position: fixed;bottom: 0;top: 0;left: 0;right: 0;background-color: rgba(0,0,0,.25);transition: background-color .25s;z-index: 1000;}' +
+		'#fba-modal{z-index: 1001;position: fixed;top:0;left: 0;right: 0;overflow-x:auto;padding:20px;}' +
+		'#fba-modal-dialog{max-width:500px;background: #fff;padding:20px;position:relative;margin:0 auto;}' +
+		'#fba-button{position: fixed;bottom: 0;right: 50px;background: #02bfe7;color: #fff;padding: 5px 10px;font-size: 1em;text-decoration: none;z-index: 999999999;}' +
+		'#fba-modal-title {margin-top: 0;margin-right: 20px;margin-bottom:20px;font-size:2.5rem;}' +
+		'#fba-text-name,#fba-text-email{max-width:100%!important;font-size:100%}' +
+		'#fba-modal-close{position: absolute;top:0;right:0;padding:10px;font-size: 24px;color: #5b616b;background: none;line-height: 1;text-decoration: none;width: auto;}' +
+		'#recruiter-form fieldset {margin: 1rem 0; }' +
+		'#recruiter-form p {margin: 1rem 0;}' +
+		'#recruiter-form label {display:block;margin: 0;}' +
+		'#recruiter-form input {margin-bottom: 1.5rem; width: 75%;}' +
+		'#recruiter-form #fba-submit {width: auto;}',
 
 		init: function( options )
 		{
@@ -20,21 +22,8 @@
 			this.loadCss();
 			this.loadHtml();
 			this.loadButton();
-			this.bindEventListeners();
-			this.dialogOpen = false;
+			this.loadOverlay();
 		},
-		bindEventListeners: function()
-	 {
-		 var self = this;
-		 d.addEventListener('keyup', function (event) {
-			 var x = event.keyCode;
-			 if( x == 27 && self.dialogOpen == true){
-			 self.closeDialog();
-		 }});
-		 d.addEventListener('click', function (event) {
-			 self.handleClick(event);
-		 });
-	 },
 		loadCss: function()
 		{
 			d.head.innerHTML += '<style>' + this.css + '</style>';
@@ -55,23 +44,20 @@
 
 			// Here lies our form interface
 			this.dialogEl.innerHTML =
-				'<form id="recruiter-form"><h3>' + this.options.title + '</h3><a id="fba-dialog-close" type="button" href="#">&times;</a>' +
-					'<p id="fba-description">' + this.options.description + '</p>' +
-				  '<fieldset>' +
-						'<label for="fba-text-name">Your first name</label><input id="fba-text-name" name="fba-text-name" class="full-name" type="text" maxlength="500">' +
-		    		'<label for="fba-text-email">Your email address</label><input id="fba-text-email" name=="fba-text-email" class="email-address" type="text" maxlength="500">' +
-						'<div class="button-wrapper">' +
-				  		'<input type="submit" id="fba-submit" class="usa-button-primary-alt" href="#"></input>' +
-						'</div>' +
-					'</fieldset>' +
-					'<p id="fba-dialog-privacy" class="usa-external_link"><a href="https://www.gsa.gov/portal/content/162010">Privacy</a></p>' +
-				'</form>';
+			'<div id="fba-modal-dialog"><h3 id="fba-modal-title">' + this.options.title + '</h3><a id="fba-modal-close" type="button" href="#">&times;</a>' +
+				'<p id="fba-description">' + this.options.description + '</p>' +
+				'<form id="recruiter-form"><fieldset>' +
+					'<label for="fba-text-name">First name</label><input id="fba-text-name" name="fba-text-name" class="full-name" type="text" maxlength="500">' +
+							'<label for="fba-text-email">Email address</label><input id="fba-text-email" name=="fba-text-email" class="email-address" type="email" maxlength="500">' +
+					'<label for="fba-text-phone">Phone number</label><input id="fba-text-phone" name="fba-text-phone" class="phone-number" type="tel" maxlength="500">' +
+					'<div class="button-wrapper">' +
+						'<button type="submit" id="fba-submit" class="usa-button-primary-alt" href="#">' + this.options.send + '</button>' +
+					'</div>' +
+				'</fieldset></form>' +
+				'<p>Note: We won’t share your information with any other organizations or groups, period. </p>' +
+				'<p id="fba-dialog-privacy" class="usa-external_link"><a href="https://www.gsa.gov/portal/content/162010">Privacy</a></p>' +
+				'</div>';
 		},
-		handleClick: function(e) {
-		if (N.fba.dialogOpen && !e.target.closest('#fba-button') && !e.target.closest('#fba-modal-dialog')) {
-		 N.fba.closeDialog();
-		}
-	},
 		handleButtonClick: function(e) { N.fba.loadDialog();e.preventDefault(); },
 		handleDialogClose: function(e) { N.fba.closeDialog();e.preventDefault(); },
 		handleSubmitClick: function(e) { N.fba.sendFeedback();e.preventDefault(); },
@@ -80,25 +66,29 @@
 			d.body.appendChild(this.buttonEl);
 			d.getElementById('fba-button').addEventListener( 'click', this.handleButtonClick, false );
 		},
+		loadOverlay: function()
+		{
+		d.body.appendChild(this.overlayEl);
+		},
 		loadDialog: function()
 		{
 			d.getElementById('fba-button').removeEventListener('click', this.handleButtonClick, false );
 			d.body.removeChild( d.getElementById('fba-button') );
 			d.body.appendChild(this.dialogEl);
-			d.getElementById('fba-dialog-close').addEventListener( 'click', this.handleDialogClose, false );
+			d.getElementById('fba-overlay').setAttribute('class','is-active');
+			d.getElementById('fba-modal').setAttribute('class','is-active');
+			d.getElementById('fba-modal-close').addEventListener( 'click', this.handleDialogClose, false );
 			d.getElementById('fba-submit').addEventListener( 'click', this.handleSubmitClick, false );
-			this.dialogOpen = true;
 		},
 		closeDialog: function()
 		{
-			d.getElementById('fba-dialog-close').removeEventListener('keypress', this.handleDialogClose, false );
-			d.getElementById('fba-dialog-close').removeEventListener('click', this.handleDialogClose, false );
+			d.getElementById('fba-modal-close').removeEventListener('click', this.handleDialogClose, false );
+			d.getElementById('fba-overlay').removeAttribute('class','is-active');
+      d.getElementById('fba-modal').removeAttribute('class','is-active');
 			d.getElementById('fba-submit').removeEventListener('click', this.handleSubmitClick, false );
-			d.body.removeChild( d.getElementById('fba-dialog') );
+			d.body.removeChild( d.getElementById('fba-modal') );
 			this.loadButton();
-			this.dialogOpen = false;
 		},
-
 		sendFeedback: function()
 		{
 			alert( this.options.thankyou );
@@ -108,12 +98,17 @@
 
 }( document, window ));
 
-// Set options for tab text, modal title, description, and alert message after submit.
-// These will be mapped to named variables in Google Tag Manager.
+/*
+ * Set options for tab text, modal title, description, and alert message after submit.
+ * These are mapped to named variables in Google Tag Manager.
+ * During development, we will occasionally load this script directly from Github.
+ * Due to scoping issues, the GTM variables will not be available to the script and cause errors.
+ */
 fba.init( {
- 'open': {{tabText}},   //'Help improve this site',
- 'title': {{formTitle}},    //'Do you have a few minutes to help us test this site?',
- 'description': {{formDesc}},   //'Hi! We’re looking for people to participate in a 30 minute phone interview about how they use [agency.gov]. If selected, we’ll contact you to set up a time.',
- 'send': {{buttonLabel}},    //'Send',
- 'thankyou': {{thankYouText}}   //'Thank you - we will be in touch shortly!'
+ 'open': {{tabText}},
+ 'title': {{formTitle}},
+ 'description': {{formDesc}},
+ 'send': {{buttonLabel}},
+ 'thankyou': {{thankYouText}}
 } );
+</script>
